80dc1ace4ce7eb8c2f610f7dc9527febdadb1cb0
Michael <mike.karpinski99@gmail.com>
2017-02-13 03:52:29 +0000

    still trying to get graph to show lines

--- client/components/index.js
+++ client/components/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Client from '../Client';
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+const Recharts = require('recharts');
+const {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
 
 class Index extends Component {
 
@@ -12,24 +13,13 @@ class Index extends Component {
 	}
 	
 	componentDidMount(){
-	  const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-    ];
 	  let objects = [];
 	  this.state.stockNames.map( (name) => {
 	    Client.get('/api/stock/' + name, (res) => {
 	      res.map( (x, index) => {
-	        console.log(name);
 	        if(!objects[index]){objects[index] = {};}
 	        objects[index]['name'] = x[0];
 	        objects[index][name] = x[1];
-	        console.log(objects);
         });
 	      this.setState({stocks: objects});
 	    });
@@ -42,15 +32,15 @@ class Index extends Component {
     		{this.state.stockNames.map( (stock, i) => {
     			return <h1 key={i}>{stock}</h1>;
     		})}
-    		<LineChart width={600} height={300} data={this.state.stocks}
+    		<LineChart width={800} height={300} data={this.state.stocks}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
           <XAxis dataKey="name"/>
           <YAxis/>
           <CartesianGrid strokeDasharray="3 3"/>
           <Tooltip/>
           <Legend />
-          <Line type="monotone" dataKey="AAPL" stroke="#8884d8" activeDot={{r: 8}}/>
-          <Line type="monotone" dataKey="AMZN" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="AAPL" stroke="#000" />
+          <Line type="monotone" dataKey="AMZN" stroke="#000" />
         </LineChart>
       </div>
     );
--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const request = require('request');
 app.use(express.static('./client/public'));
 
 app.get('/api/stock/:stock', function(req, res){
-  const url = `http://api.kibot.com/?action=history&symbol=${req.params.stock}&interval=daily&period=365`;
+  const url = `http://api.kibot.com/?action=history&symbol=${req.params.stock}&interval=daily&period=10`;
   request('http://api.kibot.com/?action=login&user=guest&password=guest', function(error, response, body){
     if(!error && response.statusCode == 200){
       request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
           const bod = body.split('\r\n');
-          const data = bod.map(val => val.split(',').slice(0,2)); 
-          console.log(data);
+          const data = bod.map(val => val.split(',').slice(0,2));
           res.end(JSON.stringify(data));
         }
       });
